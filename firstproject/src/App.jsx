e501f1dee4b38801d45dd1929223ef871d7b920d
Rishabh Shukla <rishabhshukla2901@gmail.com>
2026-03-31 07:32:10 +0000

    added three buttons

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -3,33 +3,26 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+function App(){
+function callfunction()
+        {
+            alert("function call")
+        }
+function apple(){
+  alert(" you have clicked on apple")
 }
 
+function banana(){
+  alert(" you have clicked on banana")
+}
+
+  return (
+      <>
+     <h2>welcome in new project</h2>
+     <button onClick={apple}>apple</button>
+     <button onClick={banana}>banana</button>
+     <button onClick={callfunction}>ok</button>
+     </>
+  )
+}
 export default App
