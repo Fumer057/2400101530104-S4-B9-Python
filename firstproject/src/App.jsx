ff8386ae5c77f3d81f751d86bdec8dc4aa0e2f9a
Rishabh Shukla <rishabhshukla2901@gmail.com>
2026-04-07 07:32:36 +0000

    arrow functioons

--- firstproject/src/App.jsx
+++ firstproject/src/App.jsx
@@ -4,25 +4,56 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App(){
-function callfunction()
-        {
-            alert("function call")
-        }
-function apple(){
-  alert(" you have clicked on apple")
+  let data="demo"
+function updata()
+{
+  data="example"
+  alert(data);
 }
+   function callfunction()
+         {
+             alert("function call")
+         }
+    const fruit=(name)=>
+    {
+      alert(name)
+    }
+// function callfunction()
+//         {
+//             alert("function call")
+//         }
+// function apple(){
+//   alert(" you have clicked on apple")
+// }
 
-function banana(){
-  alert(" you have clicked on banana")
-}
+// function banana(){
+//   alert(" you have clicked on banana")
+// }
+
+//   return (
+//       <>
+//      <h2>welcome in new project</h2>
+//      <button onClick={apple}>apple</button>
+//      <button onClick={banana}>banana</button>
+//      <button onClick={callfunction}>ok</button>
+//      </>
+//   )
+// }
+
+
 
   return (
       <>
-     <h2>welcome in new project</h2>
-     <button onClick={apple}>apple</button>
-     <button onClick={banana}>banana</button>
-     <button onClick={callfunction}>ok</button>
-     </>
+     {/* <h2>example of arrow function</h2>
+     <button onClick={callfunction}>click to call</button>
+     <button onClick={()=>fruit("first parameter")}>arrow function</button>
+     <button onClick={()=>fruit("click on apple")}>apple</button>
+     <button onClick={()=>fruit("click on banana")}>banana</button> */}
+     <h1>state in react</h1>
+     <h1>{data}</h1>
+     <button  onClick={updata}>click to change value</button>
+
+    </>
   )
 }
 export default App
